0e998cdd8a76ff76d1e06c364d9c69a856860f1f
Alexandre Hertzog <ale.hertzog@gmail.com>
2015-11-19 09:46:58 -0200

    Made connections to the buttons.

--- src/gui/dialog.cc
+++ src/gui/dialog.cc
@@ -3,8 +3,13 @@
 #include <wx/panel.h>
 #include <wx/sizer.h>
 #include <wx/statbox.h>
-#include <wx/spinbutt.h>
 #include <wx/button.h>
+
+/* Unfortunately, this is necessary. g++ points out many warnings about internal
+ * wxWidgets functions that are never used in our program, which causes a lot
+ * of clutter in the terminal. This fixes the problem, though we may not know if
+ * or when we'll use a deprecated function. */
+#pragma GCC diagnostic ignored "-Wdeprecated-declarations"
 
 using namespace GravSim::Gui;
 using std::string;
@@ -16,28 +21,37 @@ Dialog::Dialog(
 )
   : wxDialog(parent, -1, title)
 {
-  wxPanel *panel = new wxPanel(this, -1);
   wxBoxSizer *vbox = new wxBoxSizer(wxVERTICAL);
-  wxBoxSizer *hbox = new wxBoxSizer(wxHORIZONTAL);
 
-  wxGridSizer *labelsandfields = new wxGridSizer(fieldnames.size(), 2, 10, 10);
-  for (int i = 0; i < fieldnames.size(); i++) {
-    labelsandfields->Add(new wxStaticText(this, -1, fieldnames[i]), 0, wxEXPAND);
-    if (fieldtypes[i] == FieldType::INTEGER) {
-      labelsandfields->Add(new wxSpinButton(this), 0, wxEXPAND);
-    }
-  }
+  wxButton *okbutton = new wxButton(this, ID_OK, wxT("Ok"), wxDefaultPosition, wxSize(70, 30));
+  wxButton *cancelbutton = new wxButton(this, ID_CANCEL, wxT("Cancelar"), wxDefaultPosition, wxSize(70, 30));
 
-  wxButton *okButton = new wxButton(this, -1, wxT("Ok"), wxDefaultPosition, wxSize(70, 30));
-  wxButton *closeButton = new wxButton(this, -1, wxT("Close"), wxDefaultPosition, wxSize(70, 30));
-
-  hbox->Add(okButton, 1);
-  hbox->Add(closeButton, 1, wxLEFT, 5);
+  _sb = new wxSpinButton(this);
+  _sb->SetRange(1, 100);
+  _sb->SetValue(50);
+  wxStaticText *txt = new wxStaticText(this, -1, fieldnames[0]);
     
-  vbox->Add(panel, 1);
-  vbox->Add(labelsandfields, 2);
-  vbox->Add(hbox, 0, wxALIGN_CENTER | wxTOP | wxBOTTOM, 10);
+  vbox->Add(txt, 1, wxALIGN_CENTER, wxEXPAND);
+  vbox->Add(_sb, 1, wxALIGN_CENTER, wxEXPAND);
+  vbox->Add(okbutton, 1, wxALIGN_CENTER, wxEXPAND);
+  vbox->Add(cancelbutton, 1, wxALIGN_CENTER, wxEXPAND);
         
   SetSizer(vbox);
+
+  Connect(ID_OK, wxEVT_COMMAND_BUTTON_CLICKED, wxCommandEventHandler(
+    Dialog::OnOk 
+  ));
+  Connect(ID_CANCEL, wxEVT_COMMAND_BUTTON_CLICKED, wxCommandEventHandler(
+    Dialog::OnCancel 
+  ));
 }
 
+void Dialog::OnOk(wxCommandEvent &event) {
+  _dialogreturn = _sb->GetValue();
+  Close();
+}
+
+void Dialog::OnCancel(wxCommandEvent &event) {
+  _dialogreturn = 0;
+  Close();
+}
--- src/gui/dialog.hh
+++ src/gui/dialog.hh
@@ -1,4 +1,5 @@
 #include <wx/wx.h>
+#include <wx/spinbutt.h>
 #include <vector>
 
 namespace GravSim {
@@ -15,7 +16,17 @@ public:
     const std::vector<FieldType> fieldtypes
   );
 
+  int GetValue(void);
+
 private:
+  enum {
+    ID_OK, ID_CANCEL
+  };
+  void OnOk(wxCommandEvent &event);
+  void OnCancel(wxCommandEvent &event);
+
+  wxSpinButton *_sb;
+  int _dialogreturn;
 };
 
 } // namespace Gui
